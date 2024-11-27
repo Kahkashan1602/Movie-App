15a34b9f605be39c58f9f7d6556abec6daf3ef83
Kahkashan1602 <kahkashana85@gmail.com>
2024-11-27 20:49:20 +0530

    Movie App - initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    screens: {
+      'lg': '1440px',
+      'md': '960px',
+      'sm': '476px',
+      'xs': '376px',
+    },
+  },
+  plugins: [],
+};
+
+
